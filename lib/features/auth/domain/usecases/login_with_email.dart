bca624a0c9edba7d269c816572809b091284372e
morihb <monirhabli8@gmail.com>
2026-04-04 14:44:26 +0300

    adding more common files

---
+++ lib/features/auth/domain/usecases/login_with_email.dart
@@ -0,0 +1,25 @@
+// lib/features/auth/domain/usecases/login_with_email.dart
+import 'package:build4allgym/features/auth/domain/repository/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import '../../data/services/auth_api_service.dart';
+
+import '../entities/user_entity.dart';
+
+class LoginWithEmail {
+  final AuthRepository repository;
+  final AuthApiService authApi;
+
+  LoginWithEmail(this.repository, this.authApi);
+
+  Future<UserEntity> call({
+    required String email,
+    required String password,
+    required int ownerProjectLinkId,
+  }) {
+    return repository.loginWithEmail(
+      email: email,
+      password: password,
+      ownerProjectLinkId: ownerProjectLinkId,
+    );
+  }
+}
