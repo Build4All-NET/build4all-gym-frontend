f0893481e802cc05eb87bc6bf3af03e7d7c91c35
mounir habli <156445033+morihb@users.noreply.github.com>
2026-05-08 19:03:32 +0300

    Update admin_plans_repository.dart

--- lib/features/admin/plans/domain/repositories/admin_plans_repository.dart
+++ lib/features/admin/plans/domain/repositories/admin_plans_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/admin_plan_stats_entity.dart';
 import '../entities/admin_plan_list_item_entity.dart';
 import '../entities/admin_branch_option_entity.dart';
-import '../../data/models/createplanrequestmodel.dart';
-import '../../data/models/updateplanrequestmodel.dart';
+import '../../data/models/CreatePlanRequestModel.dart';
+import '../../data/models/UpdatePlanRequestModel.dart';
 
 abstract class AdminPlansRepository {
   Future<AdminPlanStatsEntity> getStats();
